a60b3ac13a6dfeabf4a907a1200a5eb60019780e
Fuguator <abbsvfuad@gmail.com>
2025-02-08 17:07:49 +0400

    intermediate commit

--- my-flask-app/config.py
+++ my-flask-app/config.py
@@ -1,13 +1,13 @@
 class Config:
-    # SECRET_KEY = 'your_secret_key_here'
+    SECRET_KEY = 'your_secret_key_here'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://Shelter_admin:Admin1%402024@localhost/paw_rescue'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:fuguator1@localhost/paw_rescue'
     DEBUG = True
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://Shelter_admin:Admin1%402024@localhost/paw_rescue'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:fuguator1@localhost/paw_rescue'
     DEBUG = False
 
 config = {
